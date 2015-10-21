ff4fbdcaf9027f57111f03ab19e6278af02905e3
SirNate0 <SirNate0.gmail.com>
2015-10-21 17:21:17 -0500

    Working test of xbox controls through local network created. For now, it is truly the local network, i.e. the local computer (127.0.0.1). The executable is both the server and client.

--- Headquarters/README.txt
+++ Headquarters/README.txt
@@ -1,1 +1,9 @@
 This folder is for the computer's code -- the program that will control the robot from the other location.
+
+Directories:
+Debug/
+	Contains the eclipse output files, including the Headquarters executable.
+include/
+	Contains include files for the libraries we use.
+lib/
+	Place libkNet.a and libSDL.a in this folder
---
+++ Headquarters/main.cpp
@@ -0,0 +1,293 @@
+/* Copyright The kNet Project.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
+
+/** @file HelloClient.cpp
+	@brief */
+
+#include "kNet.h"
+#include "kNet/DebugMemoryLeakCheck.h"
+
+using namespace kNet;
+
+// Define a MessageID for our a custom message.
+const message_id_t cHelloMessageID = 10;
+
+// This object gets called whenever new data is received.
+class MessageListener : public IMessageHandler
+{
+public:
+	void HandleMessage(MessageConnection *source, packet_id_t /*packetId*/, message_id_t messageId, const char *data, size_t numBytes)
+   {
+      if (messageId == cHelloMessageID)
+      {
+         // Read what we received.
+         DataDeserializer dd(data, numBytes);
+         std::cout << "Server says: " << dd.Read<bool>() << std::endl;
+
+         source->Close(0);
+      }
+   }
+};
+
+BottomMemoryAllocator bma;
+#include <SDL/SDL.h>
+#include <SDL/SDL_joystick.h>
+
+struct Joystick
+{
+	short x, y, x2, y2, l, r;
+	static const int BUTTONS = 13;
+	unsigned char buttons[BUTTONS];
+	void Read(SDL_Joystick* joy)
+	{
+		if (SDL_JoystickGetAttached(joy) == false)
+			return;
+		x = SDL_JoystickGetAxis(joy, 0);
+		y = SDL_JoystickGetAxis(joy, 1);
+		x2 = SDL_JoystickGetAxis(joy, 3);
+		y2 = SDL_JoystickGetAxis(joy, 4);
+		l = SDL_JoystickGetAxis(joy, 2);
+		r = SDL_JoystickGetAxis(joy, 5);
+		for (int i = 0; i < BUTTONS; i++)
+			buttons[i] = SDL_JoystickGetButton(joy, i);
+	}
+	void Print()
+	{
+		printf("(%d, %d), (%d, %d) [%d | %d]\n", x,y,x2,y2,l,r);
+		for (int i = 0; i < BUTTONS; i++)
+			if (buttons[i])
+				printf("%d:+ ", i);
+			else
+				printf("%d:  ", i);
+		printf("\n");
+
+	}
+//	void PrintFancy()
+//	{
+//		/*  x######			    ######X
+//		 *     X				   X
+//		 * (######,  X		 X		 X
+//		 *    X          X		   X   X
+//		 *  ######)        (######,	 X
+//		 *		------		  X
+//		 *		------	    ######)
+//		 */
+//	}
+};
+
+class JoyPrinter : public IMessageHandler
+{
+public:
+	/// Called whenever the network stack has received a message that the application
+	/// needs to process.
+	/// @param source The kNet connection this message originates from.
+	/// @param packetId A unique incrementing id counter that identifies the number of the UDP packet this message originated from. Use this
+	///     to prune out-of-order messages if necessary. kNet automatically discards duplicate messages, and can do out-of-order discarding
+	///     automatically as well, if you use message content ID's. Otherwise, you can use the packetId to do the pruning manually.
+	/// @param messageId Contains the id (or the "type") of the message. This is the one you specified when sending the message
+	/// @param data Points to the raw data buffer. This buffer may be zero if numBytes == 0.
+	/// @param numBytes The length of the raw data buffer, in bytes.
+	virtual void HandleMessage(MessageConnection *source, packet_id_t packetId, message_id_t messageId, const char *data, size_t numBytes)
+	{
+		if (numBytes >= sizeof(Joystick))
+		{
+			Joystick j = *((Joystick*) data);
+			j.Print();
+		}
+	}
+
+};
+
+// This object gets called for notifications on new network connection events.
+class ServerListener : public INetworkServerListener
+{
+	JoyPrinter jp;
+	int count;
+public:
+	ServerListener(): count(0) {}
+	void NewConnectionEstablished(MessageConnection *connection)
+	{
+//		const int maxMsgBytes = 256;
+//		// Start building a new message.
+//		NetworkMessage *msg = connection->StartNewMessage(cHelloMessageID, maxMsgBytes);
+//		msg->reliable = true;
+//
+//		// Create a DataSerializer object with a buffer of 256 bytes.
+//		DataSerializer ds(msg->data, maxMsgBytes);
+//		// Add a message string.
+//		ds.AddString(std::string("Hello! You are connecting from ") + connection->RemoteEndPoint().ToString());
+//		// Push the message out to the client.
+//		connection->EndAndQueueMessage(msg, ds.BytesFilled());
+		connection->RegisterInboundMessageHandler(&jp);
+		LOG(LogUser, "Client connected from %s.", connection->ToString().c_str());
+//		count++;
+//		if (count>100)
+//
+	}
+
+	void ClientDisconnected(MessageConnection *connection)
+	{
+		connection->Disconnect();
+	}
+};
+
+int main(int argc, char **argv)
+{
+
+
+   if (SDL_Init( SDL_INIT_JOYSTICK) < 0)
+   {
+       fprintf(stderr, "Couldn't initialize SDL joystick support: %s\n", SDL_GetError());
+       exit(1);
+   }
+   printf("%i joysticks were found.\n\n", SDL_NumJoysticks() );
+   SDL_Joystick* joy = 0;
+   Joystick j;
+   if (SDL_NumJoysticks() > 0)
+   {
+	   printf("The names of the joysticks are:\n");
+	   joy = SDL_JoystickOpen(0);
+	   j.Read(joy);
+	   j.Print();
+   }
+
+   for(int i=0; i < SDL_NumJoysticks(); i++ )
+   {
+       printf("    %s\n", SDL_JoystickName(SDL_JoystickOpen(i)));
+   }
+
+
+   printf("\n\n");
+
+   if (argc < 2)
+      {
+         std::cout << "Usage: " << argv[0] << " server-ip" << std::endl;
+         return 0;
+      }
+
+	kNet::SetLogChannels(LogUser | LogInfo | LogError);
+
+    ServerListener serverListener;
+	// Start listening on a port.
+	const unsigned short cServerPort = 1234;
+	   Network network;
+	   MessageListener listener;
+
+	NetworkServer *server = network.StartServer(cServerPort, SocketOverUDP, &serverListener, true);
+
+	if (server)
+	{
+		LOG(LogUser, "Waiting for incoming connections.");
+		// Run the main server loop.
+		// This never returns since we don't call NetworkServer::Stop(), but for this example, it doesn't matter.
+//		server->RunModalServer();
+		server->SetAcceptNewConnections(true);
+	}
+
+	EnableMemoryLeakLoggingAtExit();
+
+
+   Ptr(MessageConnection) connection = network.Connect(argv[1], cServerPort, SocketOverUDP,  &listener);
+
+	if (connection)
+	{
+		// Run the main client loop.
+//		connection->RunModalClient();
+		int i = 1;
+		while (connection)
+		{
+			DataSerializer data(sizeof(Joystick));
+			data.Add<Joystick>(j);
+			connection->SendMessage(10,true,false,100,i++,data.GetData(),data.BytesFilled());
+//					connection->RunModalClient();
+			if (i > 1000)
+				connection = 0;
+			SDL_Event EVENT;
+			SDL_PollEvent(&EVENT);
+			j.Read(joy);
+//			j.Print();
+//			sleep(1);
+			SDL_Delay(100);
+			server->Process();
+		}
+//		connection->
+	}
+
+   return 0;
+}
+
+
+
+/* Server
+
+
+#include "kNet.h"
+#include "kNet/DebugMemoryLeakCheck.h"
+
+using namespace kNet;
+
+// Define a MessageID for our a custom message.
+const message_id_t cHelloMessageID = 10;
+
+// This object gets called for notifications on new network connection events.
+class ServerListener : public INetworkServerListener
+{
+public:
+	void NewConnectionEstablished(MessageConnection *connection)
+	{
+		const int maxMsgBytes = 256;
+		// Start building a new message.
+		NetworkMessage *msg = connection->StartNewMessage(cHelloMessageID, maxMsgBytes);
+		msg->reliable = true;
+
+		// Create a DataSerializer object with a buffer of 256 bytes.
+		DataSerializer ds(msg->data, maxMsgBytes);
+		// Add a message string.
+		ds.AddString(std::string("Hello! You are connecting from ") + connection->RemoteEndPoint().ToString());
+		// Push the message out to the client.
+		connection->EndAndQueueMessage(msg, ds.BytesFilled());
+		LOG(LogUser, "Client connected from %s.", connection->ToString().c_str());
+	}
+
+	void ClientDisconnected(MessageConnection *connection)
+	{
+		connection->Disconnect();
+	}
+};
+
+BottomMemoryAllocator bma;
+
+int main()
+{
+	EnableMemoryLeakLoggingAtExit();
+
+	Network network;
+	ServerListener listener;
+
+	kNet::SetLogChannels(LogUser | LogInfo | LogError);
+
+	LOG(LogUser, "Starting server.");
+	// Start listening on a port.
+	const unsigned short cServerPort = 1234;
+	NetworkServer *server = network.StartServer(cServerPort, SocketOverUDP, &listener, true);
+
+	if (server)
+	{
+		LOG(LogUser, "Waiting for incoming connections.");
+		// Run the main server loop.
+		// This never returns since we don't call NetworkServer::Stop(), but for this example, it doesn't matter.
+		server->RunModalServer();
+	}
+}
+*/
